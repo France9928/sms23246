c5cf529f9e6dd31cad9c4fe5e14467e1c5b396aa
lucia <l.tampone1@studenti.uniba.it>
2024-02-14 15:43:40 +0100

    dichiarazioneCose

--- app/src/main/java/it/uniba/dib/sms23246/ui/video/VideoFragment.java
+++ app/src/main/java/it/uniba/dib/sms23246/ui/video/VideoFragment.java
@@ -1,29 +1,60 @@
 package it.uniba.dib.sms23246.ui.video;
 
+import static android.os.Build.VERSION_CODES.R;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.w3c.dom.Text;
+
+import it.uniba.dib.sms23246.R;
 import it.uniba.dib.sms23246.databinding.FragmentVideoBinding;
+import it.uniba.dib.sms23246.ui.shop.ShopViewModel;
 
 public class VideoFragment extends Fragment {
+
 
     private FragmentVideoBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        VideoViewModel videoViewModel =
-                new ViewModelProvider(this).get(VideoViewModel.class);
+        VideoViewModel videoViewModel = new ViewModelProvider(this).get(VideoViewModel.class);
 
         binding = FragmentVideoBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        final TextView primoT = root.findViewById(it.uniba.dib.sms23246.R.id.primoT);
+        final TextView secondoT = root.findViewById(it.uniba.dib.sms23246.R.id.secondoT);
+        final TextView terzoT = root.findViewById(it.uniba.dib.sms23246.R.id.terzoT);
+        final VideoView videoView = root.findViewById(it.uniba.dib.sms23246.R.id.videoView);
+        final SearchView searchView = root.findViewById(it.uniba.dib.sms23246.R.id.searchView);
+        final RatingBar ratingBar = root.findViewById(it.uniba.dib.sms23246.R.id.ratingBar);
+
+
+
+
+
+
+
+
+        
+
+        
+        
+
+
+  
 
         return root;
     }
