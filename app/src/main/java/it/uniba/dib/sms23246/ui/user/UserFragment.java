ad521d9eeb96e19e006ef0b35766d17f425e15a9
France9928 <f.bombace@studenti.uniba.it>
2024-02-15 11:24:12 +0100

    firestoreDB updated

--- app/src/main/java/it/uniba/dib/sms23246/ui/user/UserFragment.java
+++ app/src/main/java/it/uniba/dib/sms23246/ui/user/UserFragment.java
@@ -1,32 +1,29 @@
 package it.uniba.dib.sms23246.ui.user;
-
-import androidx.core.content.res.ResourcesCompat;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.Fragment;
 import it.uniba.dib.sms23246.R;
 import it.uniba.dib.sms23246.databinding.FragmentUserBinding;
 
 public class UserFragment extends Fragment {
     private FragmentUserBinding binding;
+    private UserViewModel userViewModel;
 
-  @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        UserViewModel userViewModel =
+        userViewModel =
                 new ViewModelProvider(this).get(UserViewModel.class);
 
         binding = FragmentUserBinding.inflate(inflater, container, false);
@@ -38,33 +35,36 @@ public class UserFragment extends Fragment {
         final TextView luogoNascitaTextView = binding.luogoNascitaTextView;
 
         // Ottiene i dati dell'utente da UserViewModel
-        LiveData<UserData> userData = UserViewModel.getUserData();
-        userData.observe(getViewLifecycleOwner(), uData -> {
-            nomeTextView.setText("Nome: " + uData.getNome());
-            cognomeTextView.setText("Cognome: " + uData.getCognome());
-            etaTextView.setText("Età: " + uData.getEta());
-            luogoNascitaTextView.setText("Luogo di nascita: " + uData.getLuogoNascita());
+        userViewModel.getUserName().observe(getViewLifecycleOwner(), nome -> {
+            nomeTextView.setText("Nome: " + nome);
         });
+
+        userViewModel.getUserLastName().observe(getViewLifecycleOwner(), cognome -> {
+            cognomeTextView.setText("Cognome: " + cognome);
+        });
+
+        userViewModel.getUserAge().observe(getViewLifecycleOwner(), age -> {
+            etaTextView.setText("Età: " + age);
+        });
+
 
         final TextView nomePatologiaTextView = binding.nomePatologiaTextView;
         final SeekBar livelloSeekBar = binding.customSeekBar;
 
-        //Ottiene i dati della patologia da UserViewModel
-        LiveData<Patologia> patologia = UserViewModel.getPatologia();
-        patologia.observe(getViewLifecycleOwner(), patologia1 -> {
-            nomePatologiaTextView.setText(patologia1.getNome());
+        // Ottiene i dati della patologia da UserViewModel
+        userViewModel.getNomePatologia().observe(getViewLifecycleOwner(), nomePatologiaTextView::setText);
 
-            int livelloValue = patologia1.getLivello().getValoreNumerico();
-            livelloSeekBar.setProgress(livelloValue);
+        userViewModel.getLivelloPatologia().observe(getViewLifecycleOwner(), livelloPatologia -> {
+            livelloSeekBar.setProgress(livelloPatologia);
 
             // Imposta il colore della SeekBar in base al valore di livello
-            if (livelloValue == 0) {
-                livelloSeekBar.setProgressDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.green_progress_bar, null));
-            } else if (livelloValue == 1) {
-                livelloSeekBar.setProgressDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.yellow_progress_bar, null));
-            } else if (livelloValue == 2) {
-                livelloSeekBar.setProgressDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.red_progress_bar, null));
+            int progressDrawableId = R.drawable.green_progress_bar;
+            if (livelloPatologia == 1) {
+                progressDrawableId = R.drawable.yellow_progress_bar;
+            } else if (livelloPatologia == 2) {
+                progressDrawableId = R.drawable.red_progress_bar;
             }
+            livelloSeekBar.setProgressDrawable(ResourcesCompat.getDrawable(getResources(), progressDrawableId, null));
         });
 
         return root;
--- app/src/main/java/it/uniba/dib/sms23246/ui/user/UserViewModel.java
+++ app/src/main/java/it/uniba/dib/sms23246/ui/user/UserViewModel.java
@@ -1,38 +1,126 @@
 package it.uniba.dib.sms23246.ui.user;
 
-import android.content.Context;
-import android.content.Intent;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
-import it.uniba.dib.sms23246.Login;
-import it.uniba.dib.sms23246.R;
+import java.util.Objects;
 
 public class UserViewModel extends ViewModel {
 
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
 
-    private static MutableLiveData<UserData> userData = new MutableLiveData<>();
-    private static MutableLiveData<Patologia> patologia = new MutableLiveData<>();
+    private MutableLiveData<String> userName = new MutableLiveData<>();
+    private MutableLiveData<Integer> userAge = new MutableLiveData<>();
+    private MutableLiveData<String> userLastName = new MutableLiveData<>();
 
-    // Metodo per caricare i dati dell'utente dal database
-    public static void loadUserDataFromDatabase(Context context, int userId) {
-       // DBHelper dbHelper = new DBHelper(context);
-        //UserData user = dbHelper.getUserData(userId);
+    private MutableLiveData<String> nomePatologia = new MutableLiveData<>();
+    private MutableLiveData<Integer> livelloPatologia = new MutableLiveData<>();
 
-        // Aggiorna il MutableLiveData con i dati dell'utente
-       // userData.postValue(user);
+    public UserViewModel() {
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        // Carica i dati dell'utente corrente quando viene istanziata la view model
+        loadUserData();
+        // Carica i dati della patologia corrente quando viene istanziata la view model
+        loadPatologiaData();
     }
 
-    public static LiveData<UserData> getUserData() {
-        return userData;
+    // Metodo per caricare i dati dell'utente corrente da Firestore
+    private void loadUserData() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+            DocumentReference userDocRef = db.collection("utenti").document(userId);
+
+            userDocRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+                    if (e != null) {
+                        // Gestisci errori
+                        return;
+                    }
+
+                    if (documentSnapshot.exists()) {
+                        // Ottieni i dati dell'utente e aggiorna la view model
+                        String name = documentSnapshot.getString("nome");
+                        Integer age = Objects.requireNonNull(documentSnapshot.getLong("eta")).intValue();
+                        String lastName = documentSnapshot.getString("cognome");
+                        String patologia = documentSnapshot.getString("patologia");
+                        Integer patologylevel = Objects.requireNonNull(documentSnapshot.getLong("livello patologia")).intValue();
+
+                        userName.setValue(name);
+                        userAge.setValue(age);
+                        userLastName.setValue(lastName);
+                        nomePatologia.setValue(patologia);
+                        livelloPatologia.setValue(patologylevel);
+
+
+
+                    }
+                }
+            });
+        }
     }
 
-    public static LiveData<Patologia> getPatologia() {
-        return patologia;
+    // Metodo per caricare i dati della patologia corrente da Firestore
+    private void loadPatologiaData() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+            DocumentReference patologiaDocRef = db.collection("patologie").document(userId);
+
+            patologiaDocRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+                    if (e != null) {
+                        // Gestisci errori
+                        return;
+                    }
+
+                    if (documentSnapshot.exists()) {
+                        // Ottieni i dati della patologia e aggiorna la view model
+                        String nomePatologia = documentSnapshot.getString("nomePatologia");
+                        Integer livelloPatologia = documentSnapshot.getLong("livelloPatologia").intValue();
+
+                        UserViewModel.this.nomePatologia.setValue(nomePatologia);
+                        UserViewModel.this.livelloPatologia.setValue(livelloPatologia);
+                    }
+                }
+            });
+        }
     }
 
+    // Metodi per ottenere i dati dell'utente
+    public LiveData<String> getUserName() {
+        return userName;
+    }
+
+    public LiveData<Integer> getUserAge() {
+        return userAge;
+    }
+
+    public LiveData<String> getUserLastName() {
+        return userLastName;
+    }
+
+
+    // Metodi per ottenere i dati della patologia
+    public LiveData<String> getNomePatologia() {
+        return nomePatologia;
+    }
+
+    public LiveData<Integer> getLivelloPatologia() {
+        return livelloPatologia;
+    }
 }
